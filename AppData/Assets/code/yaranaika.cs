1316550a3cf5ebe66ba3a8e008b8b9704bcaec9a
omegasrevenge <kirill.salzmann@web.de>
2014-11-06 14:24:02 +0100

    a little update

--- AppData/Assets/code/yaranaika.cs
+++ AppData/Assets/code/yaranaika.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class yaranaika : MonoBehaviour 
 {
+	public GameObject General;
+
 	public float Lifetime = 0.2f;
 
 	private float curAge = 0f;
@@ -11,12 +13,16 @@ public class yaranaika : MonoBehaviour
 	{
 		curAge = 0f;
 		gameObject.GetComponent<MeshRenderer> ().enabled = true;
+		General.GetComponent<MeshRenderer> ().enabled = false;
 	}
 
 	public void Update()
 	{
 		curAge += Time.deltaTime;
-		if (curAge >= Lifetime)
+		if (curAge >= Lifetime) 
+		{
 			gameObject.GetComponent<MeshRenderer> ().enabled = false;
+			General.GetComponent<MeshRenderer> ().enabled = true;
+		}
 	}
 }
