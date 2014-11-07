29851e696605900d27f4c68fec5c1ec586f779d3
omegasrevenge <kirill.salzmann@web.de>
2014-11-07 11:56:54 +0100

    prototype steht

--- AppData/Assets/code/GameManager.cs
+++ AppData/Assets/code/GameManager.cs
@@ -14,12 +14,15 @@ public class GameManager : MonoBehaviour
 	public GameObject PopUpPrefab;
 	public GameObject Yaranaika;
 	public Camera MainCamera;
+	public tk2dTextMesh TextMoney;
+	public tk2dTextMesh TextKills;
 
 	public int Money = 0;
 	public int Kills = 0;
 	public int Killrate = 0;
 
-	public int MoneyPerWeapon = 50;
+	public int MoneyPerWeaponMin = 50;
+	public int MoneyPerWeaponMax = 500;
 	public int KillratePerWeapon = 2;
 
 	private bool alreadyClicked = false;
@@ -27,6 +30,9 @@ public class GameManager : MonoBehaviour
 	void Update () 
 	{
 		Kills += Killrate;
+
+		TextMoney.text = Money.ToString();
+		TextKills.text = Kills.ToString();
 
 		if (Kills >= MAX_KILL_COUNT) 
 		{
@@ -74,7 +80,7 @@ public class GameManager : MonoBehaviour
 				if(hit.collider == Weapon.GetComponent<MeshCollider>())
 				{
 					Killrate += KillratePerWeapon;
-					Money += MoneyPerWeapon;
+					Money += Random.Range(MoneyPerWeaponMin, MoneyPerWeaponMax);
 					Instantiate(PopUpPrefab, PopUpPosition.position, PopUpPosition.rotation);
 					Yaranaika.GetComponent<yaranaika>().Activate();
 					Weapon.GetComponent<ButtonClick>().Click();
