d8d3a749cdf797136cf12fb0ddd452150977f9fb
Sadarian <tobfrey@web.de>
2014-11-11 14:13:25 +0100

    next version

--- AppData/Assets/code/GameManager.cs
+++ AppData/Assets/code/GameManager.cs
@@ -3,47 +3,52 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour 
 {
-	public const int MAX_KILL_COUNT = 1000000;
+	public const int MONEY_TO_GET = 1000000;
+	public const int WEAPONS_PER_CLICK = 10;
+	public const int MIN_MONEY_PER_WEAPON = 50;
+	public const int MAX_MONEY_PER_WEAPON = 500;
+	public const int MIN_KILL_PER_WEAPON = 1;
+	public const int MAX_KILL_PER_WEAPON = 3;
+	public const double KILL_TIMER = 2;
 
-	public GameObject ContinueButton;
-	public GameObject Page1;
-	public GameObject Page2;
-	public GameObject Page3;
 	public GameObject Weapon;
+	public GameObject Factory;
 	public Transform PopUpPosition;
 	public GameObject PopUpPrefab;
-	public GameObject Yaranaika;
 	public Camera MainCamera;
 	public tk2dTextMesh TextMoney;
-	public tk2dTextMesh TextKills;
+	public tk2dTextMesh TextWeapons;
+	//public tk2dTextMesh TextWeaponsProduction;
 
-	public int Money = 0;
-	public int Kills = 0;
-	public int Killrate = 0;
+	public float curTime;
+	public float timePast;
 
-	public int MoneyPerWeaponMin = 50;
-	public int MoneyPerWeaponMax = 500;
-	public int KillratePerWeapon = 2;
+	public int money = 0;
+	public int kills = 0;
+	public int weaponsSold = 0;
+	public int weapons = 0;
+	public int weaponsProduction = 0;
 
 	private bool alreadyClicked = false;
 
+	void Start()
+	{
+		Weapon.SetActive(false);
+	}
+
 	void Update () 
 	{
-		Kills += Killrate;
-
-		TextMoney.text = Money.ToString();
-		TextKills.text = Kills.ToString();
-
-		if (Kills >= MAX_KILL_COUNT) 
+		curTime = Time.deltaTime;
+		timePast += curTime;
+		if (timePast >= KILL_TIMER)
 		{
-			Killrate = 0;
-			Money = 0;
-			Kills = 0;
-
-			Weapon.SetActive(false);
-			Page3.SetActive(true);
-			ContinueButton.SetActive(true);
+			timePast = 0;
+			kills += Random.Range(MIN_KILL_PER_WEAPON, MAX_KILL_PER_WEAPON) * weaponsSold;
 		}
+
+		TextMoney.text = money.ToString();
+		TextWeapons.text = weapons.ToString();
+		//TextWeaponsProduction.text = weaponsProduction.ToString();
 
 		alreadyClicked = alreadyClicked ? !Input.GetMouseButtonUp (0) : alreadyClicked;
 
@@ -54,39 +59,25 @@ public class GameManager : MonoBehaviour
 			RaycastHit hit;
 			if(Physics.Raycast(MainCamera.ScreenToWorldPoint(Input.mousePosition), Vector3.forward, out hit))
 			{
-				if(hit.collider == ContinueButton.GetComponent<MeshCollider>())
-				{
-					if(Page1.activeSelf)
-					{
-						Page1.SetActive(false);
-						Page2.SetActive(true);
-						return;
-					}
-					if(Page2.activeSelf)
-					{
-						Page2.SetActive(false);
-						ContinueButton.SetActive(false);
-						Weapon.SetActive(true);
-						return;
-					}
-					if(Page3.activeSelf)
-					{
-						Page3.SetActive(false);
-						Page1.SetActive(true);
-						return;
-					}
-				}
-
 				if(hit.collider == Weapon.GetComponent<MeshCollider>())
 				{
-					Killrate += KillratePerWeapon;
-					Money += Random.Range(MoneyPerWeaponMin, MoneyPerWeaponMax);
+					money += Random.Range(MIN_MONEY_PER_WEAPON, MAX_MONEY_PER_WEAPON) * weapons;
+					weaponsSold += weapons;
+					weapons = 0;
 					Instantiate(PopUpPrefab, PopUpPosition.position, PopUpPosition.rotation);
-					Yaranaika.GetComponent<yaranaika>().Activate();
 					Weapon.GetComponent<ButtonClick>().Click();
+					Weapon.SetActive(false);
 				}
 			}
 
+			if (Physics.Raycast(MainCamera.ScreenToWorldPoint(Input.mousePosition), Vector3.forward, out hit))
+			{
+				if (hit.collider == Factory.GetComponent<MeshCollider>())
+				{
+					weapons += WEAPONS_PER_CLICK;
+					Weapon.SetActive(true);
+				}
+			}
 		}
 	}
 }
